5cf9542de48f2d7b3f399c6856faf61144e81ee0
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-18 13:33:03 +0300

    implemented sol.

--- main.kt
+++ main.kt
@@ -1,7 +1,30 @@
 fun main() {
-
+    print(maximumWealth(
+        accounts = arrayOf(
+            intArrayOf(1, 2, 3),
+            intArrayOf(3, 2, 1)
+        )
+    ))
 }
 
 fun maximumWealth(accounts: Array<IntArray>): Int {
-    
+    val rows = accounts.size
+    val cols = accounts[0].size
+
+    var i = 0
+    var maxSum = -1
+    while (i < rows) {
+        var sum = 0
+        var j = 0
+        while (j < cols) {
+            sum += accounts[i][j]
+            j++
+        }
+        if (sum > maxSum) {
+            maxSum = sum
+        }
+        i++
+    }
+
+    return maxSum   
 }
